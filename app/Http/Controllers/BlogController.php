f7c0ca184a5310a48e591e475813e47c6cc016dd
flodday <e.mpc41@gmail.com>
2024-12-21 16:02:58 +0100

    learn: MVC Blade Moteur de template

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,25 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Pagination\LengthAwarePaginator;  // Importation de LengthAwarePaginator
+use Illuminate\Pagination\Paginator;
+use Illuminate\View\View;
 
 class BlogController extends Controller
 {
-    // Mise à jour du type de retour pour accepter LengthAwarePaginator
-    public function index(): LengthAwarePaginator
-    {
-        return \App\Models\Post::paginate(25);  // paginate retourne un LengthAwarePaginator
+    public function index(): View {
+        return view('blog.index', [
+            'posts' => \App\Models\Post::paginate(1)
+        ]);
     }
 
-    public function show(string $slug, string $id): RedirectResponse | Post
+    public function show(string $slug, string $id): RedirectResponse | View
     {
-        $post = \App\Models\Post::findOrFail($id);
+        $post = Post::findOrFail($id);
         if ($post->slug !== $slug) {
             return to_route('blog.show', ['slug' => $post->slug, 'id' => $post->id]);
         }
-        return $post;
+        return view('blog.show', [
+            'post' => $post
+        ]);
     }
 }
